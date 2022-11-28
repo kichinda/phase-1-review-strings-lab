54781885b70000184fbe69fae02d02fb221db9a6
kichinda <kichindajesse@gmail.com>
2022-11-28 23:22:55 +0300

    completed assignmet

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+var currentUser=('not empty')
+
+var welcomeMessage=("Welcome to Flatbook, " + currentUser + "!");
+const excitedWelcomeMessage=("WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!");
+const shortGreeting=("Welcome, " + currentUser.slice(0,1) + "!");
